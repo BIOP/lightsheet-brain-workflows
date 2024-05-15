22483429a5caf8377adb11235dfe746e60cc17a7
Rémy Dornier <remy.dornier@epfl.ch>
2024-05-15 17:17:31 +0200

    Updade GUI to indent properly the parameters

--- YamlGuiCreator.groovy
+++ YamlGuiCreator.groovy
@@ -11,6 +11,8 @@ import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import javax.swing.*;
+import javax.swing.GroupLayout.ParallelGroup
+import javax.swing.GroupLayout.SequentialGroup
 import java.awt.FlowLayout;
 import javax.swing.BoxLayout
 import java.awt.event.ItemEvent;
@@ -32,29 +34,7 @@ return
 
 
 public class Dialog extends JFrame {
-	
-	private JComboBox<String> cmbProject;
-    private JComboBox<String> cmbDataset;
-    private JButton bnOk = new JButton("Finish");
-    private JButton bnCancel = new JButton("Cancel");
-    private JButton bnNext = new JButton("Next");
-    private DefaultComboBoxModel<String> modelCmbProject;
-    private DefaultComboBoxModel<String> modelCmbDataset;
-    	
-	//Client client;
-	def userId;
-	def project_list;
-	boolean enterPressed;
-	boolean validated;
-	File currentDir = null;
-	List<Map<String, String>> selectionList = new ArrayList<>()
-	Map<String, List<String>> projectNewDatasets = new HashMap<>()
-	String FOL_PATH = "path";
-    String OMR_PRJ = "project";
-    String OMR_DST = "dataset";
-    String IS_NEW_DST = "isNewDataset";
-    String RAW_DATA = "rawData";
-    String DST_NAME = "datasetName";
+
 	
 	public Dialog(yamlContent){
 		
@@ -64,6 +44,8 @@ public class Dialog extends JFrame {
 	// getters
 	public boolean getEnterPressed(){return this.enterPressed}
 	public boolean getValidated(){return this.validated}
+	public String getUserId(){return this.userId}
+
 	public List<Map<String, String>> getSelectedList(){return this.selectionList}
 	
 	// generate the dialog box
@@ -72,7 +54,7 @@ public class Dialog extends JFrame {
 		this.setTitle("Select your custom configuration")
 	    this.setVisible(true);
 	    this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-	    this.setPreferredSize(new Dimension(400, 250));
+	    //this.setPreferredSize(new Dimension(400, 250));
 	    
 	    // get the screen size
 	    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
@@ -85,73 +67,73 @@ public class Dialog extends JFrame {
 		JTabbedPane generalPanel = new JTabbedPane();
 		
 		yamlContent.each{
-			JPanel tabGUI = tabGUICreation(it.getValue())
-			println tabGUI
-			generalPanel.addTab(it.getKey(), null, tabGUI, "Does nothing");
+			JPanel tabPanel = new JPanel();
+			def layout = new GroupLayout(tabPanel);
+			layout.setAutoCreateGaps(true);
+        	layout.setAutoCreateContainerGaps(true);
+			
+			ParallelGroup initialHGroup = layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+			List<ParallelGroup> horizontalGroups = new ArrayList<>()
+			List<ParallelGroup> verticalGroups = new ArrayList<>()
+			horizontalGroups.add(initialHGroup)
+			
+			tabGUICreation(it.getValue(), layout, horizontalGroups, verticalGroups, initialHGroup)
+			
+			SequentialGroup horizontalGroup = layout.createSequentialGroup()
+			horizontalGroups.each{gr -> horizontalGroup.addGroup(gr)}
+			layout.setHorizontalGroup(horizontalGroup)
+			
+			SequentialGroup verticalGroup = layout.createSequentialGroup()
+			verticalGroups.each{gr -> verticalGroup.addGroup(gr)}
+			layout.setVerticalGroup(verticalGroup)
+			
+			tabPanel.setLayout(layout);
+			generalPanel.addTab(it.getKey(), null, tabPanel, "Does nothing");
 		}
 		
         this.setContentPane(generalPanel);
         this.pack();
     }
-    
-    private JPanel tabGUICreation(tabContentList){
-		JPanel tabPanel = new JPanel();
-		tabPanel.setLayout(new BoxLayout(tabPanel, BoxLayout.Y_AXIS));
-		
-		if(tabContentList != null){
-			tabContentList.each{
-		        JPanel window = new JPanel();
-		        window.setLayout(new BoxLayout(window, BoxLayout.X_AXIS));
-		        
-		        JLabel label  = new JLabel(it.getKey());
-		        window.add(label);
-		        
-		        def value = it.getValue()
-
-        		if(value instanceof Boolean){
-        			JCheckBox chk = new JCheckBox();
-    				chk.setSelected(value);
-    				window.add(chk);
-        		}else{
-        			if(value instanceof List<?>){
-        				JTextField tf = new JTextField(""+it.getValue());
-	        			tf.setColumns(15);
-	        			window.add(tf);
-        			}else{
-					    //Paths.get(value.toString())
-					    if(value.toString().contains("/") || value.toString().contains("\\")){
-						   	// Root folder for project
-					        JTextField tfFolder = new JTextField();
-					        tfFolder.setColumns(15);
+       
+    private void tabGUICreation(tabContentMap, layout, horizontalGroups, verticalGroups, labelHGroup){
+    	
+		if(tabContentMap != null){
+			
+			ParallelGroup valueHGroup
+			def index = horizontalGroups.findIndexOf{it == labelHGroup}
+			if(index == horizontalGroups.size() - 1){
+				valueHGroup = layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+				horizontalGroups.add(valueHGroup)
+			}else{
+				valueHGroup = horizontalGroups.get(index + 1)
+			}
+			
+			tabContentMap.each{
+				def value = it.getValue()
+				if(value instanceof Map<?,?>){
+					JLabel label  = new JLabel(it.getKey());
 					
-					        // button to choose root folder
-					        JButton browseFolderButton = new JButton("Choose folder");
-					        browseFolderButton.addActionListener(e->{
-					            JFileChooser directoryChooser = new JFileChooser();
-					            directoryChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-					            directoryChooser.setCurrentDirectory(currentDir);
-					            directoryChooser.setDialogTitle("Choose the project folder");
-					            directoryChooser.showDialog(new JDialog(),"Select");
+					labelHGroup.addComponent(label)
 					
-					            if (directoryChooser.getSelectedFile() != null){
-					                tfFolder.setText(directoryChooser.getSelectedFile().getAbsolutePath());
-					                currentDir = directoryChooser.getSelectedFile()
-					            }
-					        });
-	        				
-					        window.add(tfFolder);
-					        window.add(browseFolderButton);
-					    }else{
-	        				JTextField tf = new JTextField(""+it.getValue());
-		        			tf.setColumns(15);
-		        			window.add(tf);
-					    }
-        			}
-        		}
-		        tabPanel.add(window)
+					ParallelGroup VGroup = layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+					VGroup.addComponent(label)
+					verticalGroups.add(VGroup)
+					
+					tabGUICreation(value, layout, horizontalGroups, verticalGroups, valueHGroup)
+				}else{
+				
+			        JLabel label  = new JLabel(it.getKey());
+					JTextField tf = new JTextField(""+value);
+					
+					labelHGroup.addComponent(label)
+					valueHGroup.addComponent(tf)
+					
+					ParallelGroup VGroup = layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+					VGroup.addComponent(label).addComponent(tf)
+					verticalGroups.add(VGroup)
+				}
 			}
 		}
-		return tabPanel
 	}
 } 
 
